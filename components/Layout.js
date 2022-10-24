0df9f3d080a2c432ed7915213789411abc552a3e
Rahul Rawat <rahul.rawat3@byjus.com>
2022-10-24 11:01:54 +0530

    fix: :sparkles: layout for smaller device

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({children}) {
   return (<>
-    <div className="mx-5 font-poppins md:mx-auto w-1/2 ">
+    <div className="mx-5 font-poppins md:mx-auto lg:w-1/2 ">
       <Navbar />
       <main>{children}</main>
     </div>
