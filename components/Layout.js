6c3adf184a037bd5aae037172af40d77a1a0cce5
Rahul Rawat <rahul.rawat3@byjus.com>
2022-10-24 10:56:42 +0530

    feat: :sparkles: platform response changed

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Layout({children}) {
   return (<>
-    <div className="mx-5 font-poppins md:mx-auto">
+    <div className="mx-5 font-poppins md:mx-auto w-1/2 ">
       <Navbar />
       <main>{children}</main>
     </div>
